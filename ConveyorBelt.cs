f013292e320d387617d852d5ed2bfc36e51b3648
Dan Vicarel <Daniel.Vicarel@SupplyTechnologies.com>
2017-07-26 17:50:41 -0400

    Added all scripts that I've worked on so far for the SupplyTechGame

---
+++ ConveyorBelt.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace SupplyTechGame {
+
+    public enum RotationDirection {
+        Clockwise,
+        CounterClockwise,
+    }
+
+    [RequireComponent(typeof(Collider2D))]
+    public class ConveyorBelt : MonoBehaviour {
+
+        // HIDDEN FIELDS
+        private const int ROT_INCREMENT = 90;
+        private static Vector2 s_upRight = (Vector2.up + Vector2.right).normalized;
+        private static Vector2 s_upLeft = (Vector2.up + Vector2.left).normalized;
+        private static Vector2 s_downLeft = (Vector2.down + Vector2.left).normalized;
+        private static Vector2 s_downRight = (Vector2.down + Vector2.right).normalized;
+
+        private bool _rotating = false;
+        private int _angle = 0;
+        private Vector2 _direction = Vector2.zero;
+
+        // INSPECTOR FIELDS
+        [Header("Rotation Behavior")]
+        public Transform TransformToRotate;
+        public RotationDirection RotationDirection;
+        public float RotationDuration = 0.25f;
+        [Tooltip("Defines the ease-in/ease-out behavior of rotation.  X and Y axes must both go from 0 to 1.")]
+        public AnimationCurve RotationCurve;
+
+        [Header("Allowed Directions")]
+        [Tooltip("Can this object be rotated to 0° counter-clockwise?")]
+        public bool CanBe0 = true;
+        [Tooltip("Can this object be rotated to 90° counter-clockwise?")]
+        public bool CanBe90 = true;
+        [Tooltip("Can this object be rotated to 180° counter-clockwise?")]
+        public bool CanBe180 = true;
+        [Tooltip("Can this object be rotated to 270° counter-clockwise?")]
+        public bool CanBe270 = true;
+
+        // EVENT HANDLERS
+        private void Awake() {
+            // Assert that at least ONE rotation is allowed
+            bool someRotAllowed = CanBe0 || CanBe90 || CanBe180 || CanBe270;
+            Assert.IsTrue(someRotAllowed, $"A {nameof(ConveyorBelt)} must have at least one allowed rotation!");
+        }
+        private void Start() {
+            _angle = ((int)TransformToRotate.localRotation.eulerAngles.z + 360) % 360;
+            Debug.Log($"{TransformToRotate.name} has initial angle of {_angle}°");
+            _direction = getDirectionFrom(_angle);
+        }
+        private void OnDrawGizmos() {
+            float offset = 0.1f;
+            float len = 0.65f;
+            Gizmos.color = Color.black;
+            if (CanBe0) {
+                Vector2 root = (Vector2)transform.position + offset * s_upRight;
+                Gizmos.DrawLine(root, root + len * Vector2.right);
+            }
+            if (CanBe90) {
+                Vector2 root = (Vector2)transform.position + offset * s_upLeft;
+                Gizmos.DrawLine(root, root + len * Vector2.up);
+            }
+            if (CanBe180) {
+                Vector2 root = (Vector2)transform.position + offset * s_downLeft;
+                Gizmos.DrawLine(root, root + len * Vector2.left);
+            }
+            if (CanBe270) {
+                Vector2 root = (Vector2)transform.position + offset * s_downRight;
+                Gizmos.DrawLine(root, root + len * Vector2.down);
+            }
+        }
+
+        // API INTERFACE
+        public void Rotate() {
+            if (!_rotating) {
+                // Get the next rotation
+                int oldAngle = _angle;
+                int nextAngle = nextAngleFrom(oldAngle);
+                _angle = (nextAngle + 360) % 360;
+                _direction = getDirectionFrom(_angle);
+
+                // Do the rotation, unless it would just be a complete circle
+                int delta = nextAngle - oldAngle;
+                int mod = (delta + 360) % 360;
+                if (mod != 0) {
+                    Debug.Log($"Rotating {delta}° from {oldAngle} to {_angle}");
+                    StartCoroutine(doRotation(oldAngle, delta));
+                }
+            }
+        }
+        public Vector2 Direction => _direction;
+
+        // HELPERS
+        private Vector2 getDirectionFrom(int angle) => Quaternion.AngleAxis(angle, Vector3.forward) * Vector2.right;
+        private int nextAngleFrom(int oldAngle) {
+            bool good = false;
+
+            int angle = oldAngle;
+            do {
+                angle += ROT_INCREMENT * ((RotationDirection == RotationDirection.Clockwise) ? -1 : 1);
+                int mod = (angle + 360) % 360;
+                good = (
+                    (mod == 0 && CanBe0) ||
+                    (mod == 90 && CanBe90) ||
+                    (mod == 180 && CanBe180) ||
+                    (mod == 270 && CanBe270) ||
+                    (mod == 360 && CanBe0));
+            } while (!good);
+
+            return angle;
+        }
+        private IEnumerator doRotation(int oldAngle, int angleToRotate) {
+            _rotating = true;
+
+            // Rotate towards that direction each frame
+            float t = 0f;
+            float lastAngle = 0f;
+            float start = Time.time;
+            float currAngle, delta;
+            while (t < 1f) {
+                t = (Time.time - start) / RotationDuration;
+                currAngle = angleToRotate * RotationCurve.Evaluate(t);
+                delta = (currAngle - lastAngle);
+                TransformToRotate.Rotate(Vector3.forward, currAngle - lastAngle);
+                lastAngle = currAngle;
+                yield return null;
+            }
+
+            _rotating = false;
+        }
+
+    }
+
+}
---
+++ ConveyorItem.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SupplyTechGame {
+
+    [RequireComponent(typeof(Collider2D))]
+    public class ConveyorItem : MonoBehaviour {
+
+        // HIDDEN FIELDS
+        private Vector3? _target = null;
+        private Vector2 _nextDir = Vector2.zero;
+
+        // INSPECTOR FIELDS
+        [Tooltip("This is the Transform that will be moved along the conveyor.")]
+        public Transform Root;
+        public float Speed = 1f;
+        public Vector2 Direction = Vector2.zero;
+        [Tooltip("If the item's offset from the target is less than or equal to this value, then it has 'arrived'.")]
+        public float ArrivedOffset = 0.05f;
+
+        private void Update() {
+            Root.Translate(Speed * Time.deltaTime * Direction);
+            if (_target.HasValue) {
+                float newOffsetSq = Vector3.SqrMagnitude(_target.Value - Root.position);
+                if (newOffsetSq <= ArrivedOffset * ArrivedOffset)
+                    arrive();
+            }
+        }
+        private void OnTriggerEnter2D(Collider2D other) {
+            ConveyorBelt cb = other.GetComponent<ConveyorBelt>();
+            if (cb != null) {
+                _target = cb.transform.position;
+                _nextDir = cb.Direction;
+                Debug.Log("collided!");
+            }
+        }
+
+        private void arrive() {
+            Root.position = _target.Value;
+            Direction = _nextDir;
+            _target = null;
+        }
+
+
+    }
+
+}
