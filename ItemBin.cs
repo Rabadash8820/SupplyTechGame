c4ead081232928b90b8b0cf1ecc46fb875c93b13
Dan Vicarel <Daniel.Vicarel@SupplyTechnologies.com>
2017-07-26 18:25:25 -0400

    Added an ItemBin that tries to collect ConveyorItems of the correct type

---
+++ ItemBin.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace SupplyTechGame {
+
+    [RequireComponent(typeof(Collider2D))]
+    public class ItemBin : MonoBehaviour {
+
+        // INSPECTOR FIELDS
+        public ItemType Type;
+        public UnityEvent ItemCollected = new UnityEvent();
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            ConveyorItem item = other.GetComponent<ConveyorItem>();
+            if (item != null) {
+                Destroy(item.Root.gameObject);
+                bool correctType = (item.Type == Type);
+                Debug.Log($"{nameof(ItemBin)} {name} received a {(correctType ? "correct" : "wrong")} item in frame {Time.frameCount}!");
+                if (correctType)
+                    ItemCollected.Invoke();
+            }
+        }
+
+    }
+
+}
